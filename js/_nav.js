7ef101835e9f09c83dbdd61617e1a601872f26a8
Lisu <38836111+L1su@users.noreply.github.com>
2021-09-01 18:29:54 +0200

    Profil

--- js/_nav.js
+++ js/_nav.js
@@ -1,20 +1,25 @@
 //? Ukrywanie i pokazywanie menu
-let ScrollList = document.querySelector(".WarrantyList__ItemsList");
+let ScrollList1 = document.querySelector(".WarrantyList__ItemsList");
+let ScrollList2 = document.querySelector(".User__SettingList");
 let MainMenu = document.querySelector(".GW__nav");
 let DefaultPosition = 0;
 
 
 
-ScrollList.addEventListener("scroll", function(){ 
-   let st = ScrollList.scrollTop; 
-   if (st > DefaultPosition){
-      // w dól
-      MainMenu.classList.add("nav--hidden");
-      MainMenu.classList.remove("nav--show");
-   } else {
-      // w góre
-      MainMenu.classList.remove("nav--hidden");
-      MainMenu.classList.add("nav--show");
-   }
-   DefaultPosition = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-}, false);
+[ScrollList1, ScrollList2].forEach(function(item) {
+
+   item.addEventListener("scroll", function(){ 
+      let st = item.scrollTop; 
+      if (st > DefaultPosition){
+         // w dól
+         MainMenu.classList.add("nav--hidden");
+         MainMenu.classList.remove("nav--show");
+      } else {
+         // w góre
+         MainMenu.classList.remove("nav--hidden");
+         MainMenu.classList.add("nav--show");
+      }
+      DefaultPosition = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+   }, false);
+
+});
