a84585c9f3179fbd613d77dff12d0e07fd428885
Lisu <38836111+L1su@users.noreply.github.com>
2021-10-07 22:23:04 +0200

    fix menu

--- js/_nav.js
+++ js/_nav.js
@@ -1,13 +1,12 @@
 //? Ukrywanie i pokazywanie menu
-let ScrollList1 = document.querySelector(".WarrantyList__ItemsList");
+let ScrollList1 = document.querySelector(".WarrantyList__ItemsList"); 
 let ScrollList2 = document.querySelector(".User__SettingList");
 let MainMenu = document.querySelector(".GW__nav");
 let DefaultPosition = 0;
 
 
-
+//? Dodawanie chowania menu gdy scrolujemy 
 [ScrollList1, ScrollList2].forEach(function(item) {
-
    item.addEventListener("scroll", function(){ 
       let st = item.scrollTop; 
       if (st > DefaultPosition){
@@ -19,7 +18,27 @@ let DefaultPosition = 0;
          MainMenu.classList.remove("nav--hidden");
          MainMenu.classList.add("nav--show");
       }
-      DefaultPosition = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+      DefaultPosition = st <= 0 ? 0 : st; 
    }, false);
-
 });
+
+
+//? Menu i nawigacja
+const getURL = new URLSearchParams(window.location.search);
+const menuLink = getURL.get("page");
+
+switch(menuLink) {
+   case "home":
+   break;
+
+   case "user":
+      document.querySelector(".GW__User").classList.remove("User--hide");
+      document.querySelector(".nav__link").classList.remove("nav__link--active");
+      document.querySelector(`.nav__link--user`).classList.add("nav__link--active");
+   break;
+
+   // default:
+   //   console.log("default");
+ }
+
+
